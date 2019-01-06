8cbf84333edfa894c2dd1202c4738ab106912321
annie .h.h <annie2016l@gmail.com>
2019-01-06 18:36:39 +0400

    CheckPalindrome

---
+++ CheckPalindrome/CheckPalindrome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ankap
+{
+    class Program
+    {
+        static string ArrayReverse(string inputString)
+        {
+            char[] str = inputString.ToCharArray();
+            Array.Reverse(str);
+            return new string(str);
+        }
+
+        static bool checkPalindrome(string inputString)
+        {
+            return ArrayReverse(inputString) == inputString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     PALINDROME OR NOT ?");
+            Console.WriteLine("Write you experssion, please:");
+            string @string = Console.ReadLine();
+            if (checkPalindrome(@string))
+            {
+                Console.WriteLine($"{@string} is palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{@string} is not palindrome.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
