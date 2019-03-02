a713ee1ff76751101d9dddda94dbd32ba13a6d32
annie .h.h <annie2016l@gmail.com>
2019-03-03 00:10:11 +0400

    5. Shape Area

--- ShapeArea/ShapeArea/Program.cs
+++ ShapeArea/ShapeArea/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ShapeArea
 {
+    // Now we will define an n-interesting polygon. A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by 
+    // taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side.Your task is to find the area of a polygon for a given n.
+
     class Program
     {
         static int ShapeArea(int n)
