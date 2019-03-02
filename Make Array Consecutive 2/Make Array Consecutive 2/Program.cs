389ee4508dce37081daae8b3d9b9ce4f5a532ff0
annie .h.h <annie2016l@gmail.com>
2019-03-02 23:59:16 +0400

    Make Array Consecutive 2

---
+++ Make Array Consecutive 2/Make Array Consecutive 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int MakeArrayConsecutive2(int[] statues)
+        {
+            Array.Sort(statues);
+            List<int> absent = new List<int>();
+            for (int i = 0; i < statues.Length - 1; i++)
+            {
+                int a = statues[i];
+                while (statues[i + 1] - a > 1)
+                {
+                    a += 1;
+                    absent.Add(a);
+                }
+            }
+            return absent.Count();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] statues = new int[] { 6, 3, 2, 8 };
+            int count = MakeArrayConsecutive2(statues);
+
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
