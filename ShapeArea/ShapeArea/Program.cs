2570c9423a76a032355f687dac529dfe3df6f94f
annie .h.h <annie2016l@gmail.com>
2019-03-03 00:04:42 +0400

    5. ShapeArea

---
+++ ShapeArea/ShapeArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static int ShapeArea(int n)
+        {
+            return n * n + (n - 1) * (n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            string numb = Console.ReadLine();
+            int number = Convert.ToInt32(numb);
+            int shapeArea = ShapeArea(number);
+
+            Console.WriteLine(shapeArea);
+
+            Console.ReadKey();
+        }
+    }
+}
