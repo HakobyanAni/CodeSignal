7f216dab25e0961eafd4b19b274ef9a5dac5a277
annie .h.h <annie2016l@gmail.com>
2019-03-03 00:11:23 +0400

    6. Make Array Consecutive 2

--- Make Array Consecutive 2/Make Array Consecutive 2/Program.cs
+++ Make Array Consecutive 2/Make Array Consecutive 2/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
+    // Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue
+    // having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them 
+    // from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may 
+    // need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
     class Program
     {
         static int MakeArrayConsecutive2(int[] statues)
